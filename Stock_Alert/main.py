da6d3523cbb186f9584da6787bb814a139cc5a57
sasha_cz <alexandra.pietroch@gmail.com>
2023-11-08 12:49:32 +0100

    update commit

--- Stock_Alert/main.py
+++ Stock_Alert/main.py
@@ -1,4 +1,4 @@
-from response import Response
+from errorhandling import ErrorHandling
 from decimal import Decimal
 import os
 
@@ -12,14 +12,21 @@ NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 news_api_key = os.environ.get("NEWS_API_KEY")
 
 # Get hold of the API endpoint of Alphavantage.
-stock_request = Response(STOCK_ENDPOINT)
-stock_response = stock_request.get_hold_of(function="TIME_SERIES_DAILY", symbol=STOCK_SYMBOL, outputsize="compact", apikey=stock_api_key)
+stock_params = {
+    "function": "TIME_SERIES_DAILY",
+    "symbol": STOCK_SYMBOL,
+    "outputsize": "compact",
+    "apikey": stock_api_key
+}
+
+stock_request = ErrorHandling(STOCK_ENDPOINT)
+stock_response = stock_request.get_hold_of(**stock_params)
 
 if stock_response:
     stock_data = stock_response.json()["Time Series (Daily)"]
 
     # Get hold of the actual yesterday's stock_data (independent of the actual date).
-    data_list = [value for (key, value) in stock_data.items()]
+    data_list = list(stock_data.values())
     yesterday_data = data_list[0]
     day_before_yesterday_data = data_list[1]
 
@@ -37,15 +44,23 @@ if stock_response:
     # If the stock price increases/decreases by 5% between yesterday and the day before yesterday,
     # then get the latest three news articles.
     if percentage >= 5 or percentage <= -5:
-        news_request = Response(NEWS_ENDPOINT)
-        news_response = news_request.get_hold_of(apiKey=news_api_key, qInTitle=COMPANY_NAME, language="en",
-                                                 sortBy="publishedAt")
+
+        news_params = {
+            "apiKey": news_api_key,
+            "qInTitle": COMPANY_NAME,
+            "language": "en",
+            "sortBy": "publishedAt",
+        }
+
+        news_request = ErrorHandling(NEWS_ENDPOINT)
+        news_response = news_request.get_hold_of(**news_params)
 
         if news_response:
             three_articles = news_response.json()["articles"][:3]
             list_articles = [f"Headline: {article['title']}\nBrief: {article['description']}\n"
                              f"Read the full article here: {article['url']}" for article in three_articles]
             print(f"{list_articles[0]}\n\n{list_articles[1]}\n\n{list_articles[2]}")
+
 
     # You can modify this code using the 'smtplib' module and store the code in a cloud (e.g. pythonanywhere)
     # so that you can send yourself daily E-Mail Alerts.
