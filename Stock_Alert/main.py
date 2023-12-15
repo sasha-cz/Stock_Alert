de41f68ba0d8fee1b8b7f1b0e64103753e6e963b
sasha_cz <alexandra.pietroch@gmail.com>
2023-12-15 16:18:20 +0100

    reorganize into functions

--- Stock_Alert/main.py
+++ Stock_Alert/main.py
@@ -1,4 +1,4 @@
-from request import Request
+import requests
 from news_request import get_news_data, show_latest_articles, COMPANY_NAME
 from decimal import Decimal
 import os
@@ -6,50 +6,65 @@ import os
 STOCK_SYMBOL = "OTLY"
 STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 
-stock_api_key = os.environ.get("STOCK_API_KEY")
 
-# Get hold of the API endpoint of Alphavantage.
-stock_params = {
-    "function": "TIME_SERIES_DAILY",
-    "symbol": STOCK_SYMBOL,
-    "outputsize": "compact",
-    "apikey": stock_api_key
-}
+def get_stock_data():
+    stock_api_key = os.environ.get("STOCK_API_KEY")
 
-stock_response = Request(STOCK_ENDPOINT).make(stock_params).json()
+    # Get hold of the API endpoint of Alphavantage.
+    stock_params = {
+        "function": "TIME_SERIES_DAILY",
+        "symbol": STOCK_SYMBOL,
+        "outputsize": "compact",
+        "apikey": stock_api_key
+    }
 
+    stock_response = requests.get(STOCK_ENDPOINT, params=stock_params).json()
 # When using a wrong API key for the API endpoint of Alpha Vantage,
 # it currently returns still the status code 200.
 # The following workaround catches the wrong input as long as the stock_response contains the key 'Error Message'.
-if 'Error Message' in stock_response:
-    raise KeyError(f"{stock_response['Error Message']}")
-else:
-    stock_data = stock_response["Time Series (Daily)"]
+    if 'Error Message' in stock_response:
+        raise KeyError(f"{stock_response['Error Message']}")
+    else:
+        return stock_response["Time Series (Daily)"]
 
-# Get hold of yesterday's stock_data (independent of the actual date).
-    data_list = list(stock_data.values())
-    yesterday_data = data_list[0]
-    day_before_yesterday_data = data_list[1]
 
-# (after the structure of the requested data)
+stock_data = get_stock_data()
+
 
 # Get hold of the closing price of yesterday and the day before yesterday.
-    price_yesterday = yesterday_data["4. close"]
-    price_day_before_yesterday = day_before_yesterday_data["4. close"]
-    recent_closing_price = Decimal(price_yesterday)
+def get_closing_price_for_yesterday_and_day_before_yesterday():
+    data_list = list(stock_data.values())
+    price_yesterday = data_list[0]["4. close"]
+    price_day_before_yesterday = data_list[1]["4. close"]
+    closing_price_yesterday = Decimal(price_yesterday)
     previous_closing_price = Decimal(price_day_before_yesterday)
+    return closing_price_yesterday, previous_closing_price
+
+
+closingPriceYesterday, previousClosingPrice = get_closing_price_for_yesterday_and_day_before_yesterday()
+
 
 # Get the current difference between the recent closing price and the previous closing price in percentage.
-    percentage_change = ((recent_closing_price - previous_closing_price) / previous_closing_price) * 100
+def calculate_percentage():
+    percentage_change = ((closingPriceYesterday - previousClosingPrice) / previousClosingPrice) * 100
     percentage = round(percentage_change, 2)
-    print(f"My Stock Alert\n\n{COMPANY_NAME}: {percentage} %\n\n")
+    return percentage
+
+
+percentage_stock = calculate_percentage()
+
+print(f"My Stock Alert\n\n{COMPANY_NAME}: {percentage_stock} %\n\n")
+
 
 # If the stock price increases/decreases by 5% between yesterday and the day before yesterday,
 # then get the latest three news articles.
-    if percentage >= 5 or percentage <= -5:
+def add_latest_news():
+    if percentage_stock >= 5 or percentage_stock <= -5:
         get_news_data()
-        print(show_latest_articles())
+        return show_latest_articles()
 
+
+print(add_latest_news())
 
 # You can modify this code using the 'smtplib' module and store the code in a cloud (e.g. pythonanywhere)
 # so that you can send yourself daily E-Mail Alerts.
@@ -61,10 +76,10 @@ else:
 # password = "mypassword"
 
 # Get hold of the smtp address for your email provider:
-    #   with smtplib.SMTP("smtp.mailprovider.com", port="myportnumber") as connection:
-    #       connection.starttls()
-    #       connection.login(user=my_email, password=password)
-    #       connection.sendmail(
-    #       from_addr=my_email, to_addrs="recipient@mailprovider.com", \
-    #           msg=f"Subject: My Stock Alert\n\n{COMPANY_NAME}: {percentage}"
-    #       f"\n\n{list_articles[0]}\n\n{list_articles[1]}\n\n{list_articles[2]}")
+#   with smtplib.SMTP("smtp.mailprovider.com", port="myportnumber") as connection:
+#       connection.starttls()
+#       connection.login(user=my_email, password=password)
+#       connection.sendmail(
+#       from_addr=my_email, to_addrs="recipient@mailprovider.com", \
+#           msg=f"Subject: My Stock Alert\n\n{COMPANY_NAME}: {percentage}"
+#       f"\n\n{list_articles[0]}\n\n{list_articles[1]}\n\n{list_articles[2]}")
